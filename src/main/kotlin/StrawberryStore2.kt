a2a2548719767153abc47dd085be6ac366380f2d
DisterDE <disterko@gmail.com>
2024-11-22 23:18:26 +0100

    Refactor `buy` method to improve clarity and validation
    Refactored the `buy` method to include bill validation and more precise change calculation logic. Introduced a set for allowed bills and a private constant for price to enhance code readability and maintainability. Also, fixed some minor formatting issues in the comments.

--- src/main/kotlin/StrawberryStore2.kt
+++ src/main/kotlin/StrawberryStore2.kt
@@ -1,43 +1,49 @@
+import kotlin.collections.setOf
+
 fun main() {
     val res = StrawberryStore2().buy(intArrayOf(5, 10, 5, 20))
     val res2 = StrawberryStore2().buy(intArrayOf(5, 20, 5, 20))
     val res3 = StrawberryStore2().buy(intArrayOf(5, 5, 10, 10, 20))
     println(res) // true
     println(res2) // false
-    println(res3) //false
+    println(res3) // false
 }
 
 
 class StrawberryStore2 {
     private val bank = IntArray(3)
+    private val allowedBills = setOf(5, 10, 20)
 
     fun buy(bills: IntArray): Boolean {
         require(bills.isNotEmpty()) { "No bills" }
         for (bill in bills) {
-            when (bill) {
-                5 -> bank[0]++
-                10 -> {
-                    if (bank[0] == 0) return false
-                    bank[0]--
-                    bank[1]++
-                }
 
-                20 -> {
-                    if (bank[1] > 0 && bank[0] > 0) {
-                        bank[1]--
-                        bank[0]--
-                    } else if (bank[0] >= 3) {
-                        bank[0] -= 3
-                    } else {
-                        return false
-                    }
-                    bank[2]++
-                }
-
-                else -> throw IllegalArgumentException( "Wrong bill: $bill, only 5, 10 and 20 Euro allowed" )
+            require(bill in allowedBills) {
+                throw IllegalArgumentException("Wrong bill: $bill, only 5, 10 and 20 Euro allowed")
             }
+
+            val bankIndex = bill / 10
+            bank[bankIndex]++
+
+            var change = bill - PRICE
+
+            if (change == 0) continue
+
+            if (change > 10 && bank[1] > 0) {
+                bank[1]--
+                change %= 10
+            }
+
+            val fives = change / 5
+            if (fives > bank[0]) return false
+
+            bank[0] -= fives
         }
         return true
+    }
+
+    companion object {
+        private const val PRICE = 5
     }
 }
 
